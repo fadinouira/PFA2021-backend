c3611cc6857680cb0e96114c0432978a78a80ce3
fadinouira <fadi.nouira1@gmail.com>
2021-04-16 01:35:48 +0200

    delivery model

---
+++ models/delivery.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const unique = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+  email : {type: String, required : true, unique:true},
+  password : {type:String , required : true},
+  name : {type: String , required : true },
+  city : {type: String , required : true },
+  location :{type: String , required : false },
+  phone : {type: String, required : true, unique:true},
+});
+
+userSchema.plugin(unique);
+
+module.exports = mongoose.model('User',userSchema);
+
