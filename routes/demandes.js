c2b37c8f67bcb6bf3440f81456d36c14076c0ad8
fadinouira <fadi.nouira1@gmail.com>
2021-04-19 02:48:48 +0200

    fixed demandes.js

--- routes/demandes.js
+++ routes/demandes.js
@@ -33,6 +33,118 @@ router.post("",checkAuth,(req,res,next)=> {
 
 })
 
+//update a demande
+router.put("/:id",checkAuth, (req, res, next) => {
+  const demande = new Demande({
+    _id: req.params.id,
+    originAddress : req.body.originAddress,
+    deliveryAddress :req.body.deliveryAddress,
+    wantedArrivalDate : req.body.wantedArrivalDate,
+    itemShipped : req.body.itemShipped,
+    itemDelivered : req.body.itemDelivered,
+  }) ;
+  Demande.updateOne({ _id: req.params.id , owner : req.userData.id}, demande).then(result => {
+    if(result.nModified > 0){
+      res.status(200).json({ message: "Update successful!" });
+    }
+    else {
+      res.status(401).json({ message: "only the owner can modify this !" });
+    }
+  });
+});
+
+//get all demandes
+router.get('',(req,res,next)=> {
+  console.log(req.query);
+  const pageSize = +req.query.pageSize ;
+  const currentPage = +req.query.currentPage ;
+  const demandeQuery = Demande.find() ;
+  let fetchedDemandes;
+  if(pageSize && currentPage) {
+    demandeQuery
+      .skip(pageSize * (currentPage - 1))
+      .limit(pageSize);
+  }
+
+  demandeQuery
+    .then(documents => {
+      fetchedDemandes = documents ;
+      return Demande.countDocuments();
+    })
+      .then(count => {
+        res.status(200).json({
+          message : "result from server:",
+          demandes: fetchedDemandes,
+          maxPages : count
+        });
+      })
+    .catch();
+});
+
+//get one demande
+router.get('/:id',(req,res,next)=> {
+  Demande.findOne({_id : req.params.id}).then((result) => {
+    console.log(result);
+    if(result){
+      res.status(200).json({ 
+        message:  'delivery received',
+        demande : result
+      });
+    }
+    else {
+      res.status(404).json({ message: "Demande does not exist !" });
+    }
+  });
+
+});
+
+//delete one demande
+router.delete('/:id',checkAuth,(req,res,next) => {
+  Demande.deleteOne({_id : req.params.id}).then((result)=>{
+    console.log(result);
+    if(result.deletedCount > 0){
+      res.status(200).json({ message:  'Demande deleted !'});
+    }
+    else {
+      res.status(401).json({ message: "only the owner can delete this !" });
+    }
+  });
+});
+
+//when transporter request to take this demande
+router.put("/takeJob/:id",checkAuth, (req, res, next) => {
+  demande = {
+    _id: req.params.id,
+    $push : {listedProviders : req.body.provider} ,
+  };
+  console.log(demande);
+  Demande.updateOne({ _id: req.params.id }, demande).then(result => {
+    if(result.nModified > 0){
+      res.status(200).json({ message: "provider listed successfully" });
+    }
+    else {
+      res.status(401).json({ message: "Failed !" });
+    }
+  });
+});
+
+//when transporter request to take this demande 
+router.put("/acceptJob/:id",checkAuth, (req, res, next) => {
+  demande = {
+    _id: req.params.id,
+    provider : req.body.provider
+  };
+  console.log(demande);
+  Demande.updateOne({ _id: req.params.id,owner : req.userData.id }, demande).then(result => {
+    if(result.nModified > 0){
+      res.status(200).json({ message: "provider accepted successfully" });
+    }
+    else {
+      res.status(401).json({ message: "Failed !" });
+    }
+  });
+});
+
 
 
 module.exports = router ;
